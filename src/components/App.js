b85fe3a3fc61b10aa3fd8d8522b5b463f27a4233
Mercedes <meegido@gmail.com>
2020-04-16 13:03:15 +0200

    added basic components and their styles

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
+import FeaturedMix from './FeaturedMix';
+import Header from './Header';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Marmalade</h1>
+class App extends Component {
+  render() {
+    return(
+    <div>
+      <div className="flex-l justify-end">
+        {/* FeaturedMix */}
+        <FeaturedMix />
+        <div className="w-50-l relative z-1">
+          {/* Header */}
+          <Header/>
+          {/* Routed Pages */}
+        </div>
+      </div>
+      <div>
+        {/* AudioPlayer */}
+          <iframe 
+            width="100%" 
+            height="60" 
+            src="https://www.mixcloud.com/widget/iframe/?hide_cover=1&mini=1&feed=%2Fjuanfalibene%2Fsounds-healing-mixtape-2%2F" 
+            frameBorder="0" 
+            className="db fixed bottom-0 z-5" 
+            />
+      </div>
     </div>
-  );
+    )
+  }
 }
 
 export default App;
---
+++ src/components/FeaturedMix.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const FeaturedMix = props => (
+  <div className="w-50-l vh-100 flex items-center justify-center cover bg-center bg-featured pad-bottom fixed-l left-0">
+    <div className="w-100 tc pa3">
+      <p className="b biryani f6 white ttw">Featured mix</p>
+      <h1 className="mix-title mt0 mb2 anton white ttu">Mint condition w/ Hottohobo - 27 November 2017</h1>
+      {/* PlayButton */}
+    </div>
+  </div>
+);
+
+export default FeaturedMix;
