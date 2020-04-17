ab9045e9920c505a0f831684f576ae087ccec1b6
Mercedes <meegido@gmail.com>
2020-04-17 19:35:41 +0200

    active stile with NavLink

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = props => (
     <h1 className="ttu f3 tracked-mega anton tc mt0 mb3">Marmalade.fm</h1>
     <ul className="list flex justify-center pl0">
       <li className="mh2">
-        <NavLink to="/" className="nav-link link biryani-black f6 ttu gray">What's hot</NavLink>
+        <NavLink exact to="/" className="nav-link link biryani-black f6 ttu gray">What's hot</NavLink>
       </li>
       <li className="mh2">
         <NavLink to="/archive" className="nav-link link biryani-black f6 ttu gray">Archive</NavLink>
