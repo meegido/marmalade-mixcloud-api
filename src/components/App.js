98fb980838809ccd956c408acd902d9b99e51d73
Mercedes <meegido@gmail.com>
2020-05-04 11:45:30 +0200

    changing playbutton based on id through css

--- src/components/App.js
+++ src/components/App.js
@@ -27,7 +27,7 @@ class App extends Component {
     this.widget = Mixcloud.PlayerWidget(this.player.current);
     await this.widget.ready;
 
-     this.widget.events.pause.on(() =>
+    this.widget.events.pause.on(() =>
       this.setState({
         playing: false,
       })
@@ -42,7 +42,7 @@ class App extends Component {
   }
 
   componentDidMount() {
-    this.mountAudio();    
+    this.mountAudio();  
   }
 
   actions = {
@@ -81,14 +81,14 @@ class App extends Component {
         </div>
         <div>
           {/* AudioPlayer */}
-            <iframe 
-              width="100%" 
-              height="60" 
-              src="https://www.mixcloud.com/widget/iframe/?hide_cover=1&mini=1&feed=%2Fjuanfalibene%2Fsounds-healing-mixtape-2%2F" 
-              frameBorder="0" 
-              className="db fixed bottom-0 z-5"
-              ref={this.player} 
-            />
+          <iframe 
+            width="100%" 
+            height="60" 
+            src="https://www.mixcloud.com/widget/iframe/?hide_cover=1&mini=1&feed=%2Fjuanfalibene%2Fsounds-healing-mixtape-2%2F" 
+            frameBorder="0" 
+            className="db fixed bottom-0 z-5"
+            ref={this.player} 
+          />
         </div>
       </div>
     </Router>
--- src/components/Mix.js
+++ src/components/Mix.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import PlayButton from './PlayButton';
+import PlayMix from './PlayMix';
 
-const Mix = ({name, playMix, currentMix}) => (
-  <div className="aspect-ratio aspect-ratio--3x4 pointer bg-black" 
-       onClick={() => playMix('/TheVinylFactory/vf-live-joakim/')}>
-    <div className="ph3 pv4 aspect-ratio--object mix-overlay">
-      <div className="flex items-center relative z-2">
-        <h1>currently playing: {currentMix}</h1>
-        <h1 className="f4 f3-l mv0 white ttu biryani pr2 lh-title">{name}</h1>
-        <PlayButton />
+const Mix = ({name, ...props}) => (
+  <div className="aspect-ratio aspect-ratio--3x4 pointer bg-black">
+    <PlayMix {...props}>
+      <div className="ph3 pv4 aspect-ratio--object mix-overlay">
+        <div className="flex items-center relative z-2">
+          <h1 className="f4 f3-l mv0 white ttu biryani pr2 lh-title">{name}</h1>
+          <PlayButton />
+        </div>
       </div>
-    </div>
+    </PlayMix>
   </div>
 );
 
