781266534a2e7f6ad2e075a0dfaf9695db95db79
Mercedes <meegido@gmail.com>
2020-05-08 12:22:17 +0200

    stats moved to another component

--- src/components/Show.js
+++ src/components/Show.js
@@ -1,33 +1,42 @@
 import React, {Component} from 'react';
-
+import Stat from './Stat';
 class Show extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      mix: null
+      mix: {}
     }
   }
   
   componentWillReceiveProps(nextProps) {
     const {match} = this.props;
     const {mixes} = nextProps;
+    console.log({mixes})
 
-    const [firstMix = {}] = mixes.filter(mix => mix.slug === match.params.slug)
+    const [firstMix = {}] = mixes.filter(mix => mix.slug === match.params.slug);
+    console.log(firstMix)
     this.setState({
       mix: firstMix
-    })  
+    }) 
   }
 
   render () {
     const {match} = this.props;
+    const {mix} = this.state;
+    
     return (
-      <div>
-        <h1>Show Page</h1>
-        <p>{match.params.slug}</p>
+      <div className="ph3 ph4-l pad-bottom">
+        <div className="measure center lh-copy">
+          <p>{mix.description}</p>
+        </div>
+        <div>
+          <Stat statName="Plays" statNumber={54654} statWord="times"/>
+          <Stat statName="Uploaded" statNumber={mix.created_time} statWord="days ago"/>
+          <Stat statName="Lasting for" statNumber={mix.audio_length / 60} statWord="minutes"/>
+        </div>
       </div>
     )
   }
-
 }
 
 export default Show;
