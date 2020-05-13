21b7507601a58378a1abf6582a2a3643d102d5bc
Mercedes <meegido@gmail.com>
2020-05-13 12:10:16 +0200

    some style details and linking header to home

--- src/components/About.js
+++ src/components/About.js
@@ -2,16 +2,13 @@ import React from 'react';
 import Stat from './Stat';
 
 const About = ({mixes}) => (
-  <div className="measure center">
-    <div className="lh-copy mb4">
+  <div className="ph3 ph4-l pad-bottom">
+    <div className="measure center lh-copy">
       <p className="mt0"> Marmalade.fm features the latest and greatest in grooves, beats and world music.</p>
-      <p className="">
+      <p className="mb4">
         Whether you’re into hip hop, trip hop, classic jazz, fusion jazz, afro beat or break beat…
         we have you covered!
       </p>
-    </div>
-
-    <div className="">
       <Stat statName="Featuring..." statNumber={mixes.length} statWord="mixes"/>
       <Stat statName="Played..." statNumber={mixes.reduce((accumulator, current) => accumulator + current.play_count, 0)} statWord="times"/>
       <Stat statName="Width..." statNumber={mixes.reduce((accumulator, current) => accumulator + current.audio_length, 0)} statWord="seconds"/>
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Mix from './Mix';
 
 const Home = ({mixes, ...props}) => (
-  <div className='flex flex-wrap justify-between mixes ph3 ph4-1'>
+  <div className='flex flex-wrap justify-between mixes ph3 ph4-1 mb5'>
     {mixes.slice(0, 6).map((mix, index) => (
       <div className='mix mb4'>
         <Mix {...props} {...mix} id={mix.key} key={index}/>
