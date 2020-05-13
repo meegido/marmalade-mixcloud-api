785f926e29147b9b381a51b218c59535a1cc3a11
Mercedes <meegido@gmail.com>
2020-05-13 11:33:29 +0200

    added tags inside show component

--- src/components/Show.js
+++ src/components/Show.js
@@ -2,6 +2,20 @@ import React, {Component} from 'react';
 import differenceInDays from 'date-fns/differenceInDays';
 import parseISO from 'date-fns/parseISO'
 import Stat from './Stat';
+
+const Tag = ({ name, url}) => (
+  <div className="mr2 mb2 o-70" >
+    <a href={url}   className="block f6 link blue b ba bw1 b--blue gray br2 pv1 ph2 lh-title">   
+      {name}
+    </a>
+  </div>
+)
+
+const Tags = ({tags = []}) => (
+  <div className="tags flex flex-wrap">
+    {tags.map(tag => <Tag {...tag} />)}
+  </div>
+)
 class Show extends Component {
   render () {
     // const {match} = this.props;
@@ -11,6 +25,7 @@ class Show extends Component {
     return (
       <div className="ph3 ph4-l pad-bottom">
         <div className="measure center lh-copy">
+          <Tags tags={mix.tags}/>
           <p>{mix.description}</p>
         </div>
         <div>
