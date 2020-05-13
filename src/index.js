f507d23fc345c7fffe4342d559a4375a38eb5c0d
Mercedes <meegido@gmail.com>
2020-05-13 12:44:24 +0200

    created provider at index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { Provider } from 'react-redux'
+import store from './redux/store'
+
 import 'tachyons';
 import './css/main.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+const Root = () => (
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
+);
+
+ReactDOM.render(<Root/>,
   document.getElementById('root')
 );
+
+// const rootElement = document.getElementById('root')
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <TodoApp />
+//   </Provider>,
+//   rootElement
+// )
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
